fc2097d8514c5424fd48c31292caa15c851cb835
Seth David Gyimah <esec26@gmail.com>
2022-07-23 22:02:56 +0000

    state and event code along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  
+  function setPower(){
+    setIsOn((isOn) => !isOn)
+    
+  }
+const color = isOn ?  "red" : "blue"
+  
+  return (
+    <button style={{ background: color }} onClick={setPower}>
+    {isOn ? "ON" : "OFF"}
+  </button>
+  )
 }
 
 export default Toggle;
